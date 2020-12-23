a91012e50430270b369125a2fd6179207e3f1e8b
Kavya-Gowtham <kavyanagaraj20596@gmail.com>
2020-12-23 10:46:12 +0530

    Test two numbers

--- src/main/java/com/string/calculator/StringCalculator.java
+++ src/main/java/com/string/calculator/StringCalculator.java
@@ -6,7 +6,28 @@ public class StringCalculator {
         if (text.isEmpty()) {
             return 0;
         } else {
-            return Integer.parseInt(text);
+            String delimiter = ",";
+
+            String numList[] = splitNumbers(text, delimiter + "|\n");
+            return sum(numList);
         }
+    }
+
+    public static int toInt(String number){
+        return Integer.parseInt(number);
+    }
+    public static String[] splitNumbers(String numbers,String divider){
+        return numbers.split(divider);
+    }
+
+    private static int sum(String[] numbers) {
+        int total = 0;
+        String tempString = "";
+        for (String number:numbers){
+                tempString = tempString + ("," + number);
+            total += toInt(number);
+        }
+
+        return total;
     }
 }
